3534d0cb0b5e44429c295ff1109c3b4c275ce980
Zaki Abdelwahed <zakiabdelwahed@gmail.com>
2021-05-20 08:25:12 +0200

    Test de résolution du problème d'encodage des noms de Moughataa

--- Dashboard/utils/loadGeojson.py
+++ Dashboard/utils/loadGeojson.py
@@ -4,4 +4,8 @@ def LoadGeojson(file_name):
     # Lecture du fichier GeoJSON
     file = open(file_name)
     gj = json.load(file)
+   # for i in range(len(gj['features'])):
+   #     name = gj['features'][i]['properties']['NAME_2']
+   #     name_corrected = name.encode("latin_1").decode("utf_8")
+   #     gj['features'][i]['properties']['NAME_2'] = name_corrected
     return(gj)
